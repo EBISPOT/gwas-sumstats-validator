58614de534a8be9a1f7e0e9fd7efbed249a6fb0d
jdhayhurst <hayhurst.jd@gmail.com>
2019-09-13 09:56:50 +0100

    cleaning up

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
 from setuptools import setup
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='ss-validate',
-    version='0.1-SNAPSHOT',
+    description='GWAS summary statistics file validator',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    version='0.1.0',
     packages=['validate'],
+    license='Apache License, Version 2.0',
     entry_points={
         "console_scripts": ['ss-validate = validate.validator:main']
     },
-    url='https://github.com/EBISPOT/sum-stats-formatter',
-    license='',
+    url='https://github.com/EBISPOT/gwas-sumstats-validator',
+    author='EBI SPOT',
+    author_email='gwas-info@ebi.ac.uk',
     install_requires=['pandas_schema']
 )
